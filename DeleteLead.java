2986a9dedfb8171e596c45181a98f524def3b502
Monishakmssy <109016609+Monishakmssy@users.noreply.github.com>
2022-07-29 16:41:59 +0530

    Update DeleteLead.java

--- DeleteLead.java
+++ DeleteLead.java
@@ -22,10 +22,8 @@ public class DeleteLead {
 		driver.findElement(By.partialLinkText("CRM/SFA")).click();
 		driver.findElement(By.partialLinkText("Leads")).click();
 		driver.findElement(By.partialLinkText("Find Leads")).click();
-		driver.findElement(By.xpath("//span[text()='Phone']")).click();
-//		driver.findElement(By.xpath("//input[@name='phoneCountryCode']")).sendKeys("1");
-//		driver.findElement(By.xpath("//input[@name='phoneAreaCode']")).sendKeys("1");
-		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("9943082610");
+		driver.findElement(By.xpath("//span[text()='Email']")).click();
+		driver.findElement(By.xpath("//input[@name='emailAddress']")).sendKeys("monisha602@gmail.com");
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
 		Thread.sleep(1000);
 		WebElement lead1 = driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId'])[1]/a"));

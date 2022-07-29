1bae6a731caa774d08c3893d50b08976b187a8d7
Monishakmssy <109016609+Monishakmssy@users.noreply.github.com>
2022-07-29 16:35:38 +0530

    Update DeleteLead.java

--- DeleteLead.java
+++ DeleteLead.java
@@ -16,33 +16,37 @@ public class DeleteLead {
 	    driver.get("http://leaftaps.com/opentaps/control/main");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("demosalesmanager");
-		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("crmsfa");
-		driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'CRM/SFA')]")).click();
-		driver.findElement(By.xpath("//a[text()='Leads']")).click();
-		driver.findElement(By.xpath("//a[text()='Find Leads']")).click();
-		driver.findElement(By.xpath("//span[text()='Email']")).click();
-        driver.findElement(By.xpath("//input[@name='emailAddress']")).sendKeys("monisha602@gmail.com");
-        String title = driver.getTitle();
-		System.out.println(title);
+				driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.partialLinkText("CRM/SFA")).click();
+		driver.findElement(By.partialLinkText("Leads")).click();
+		driver.findElement(By.partialLinkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Phone']")).click();
+//		driver.findElement(By.xpath("//input[@name='phoneCountryCode']")).sendKeys("1");
+//		driver.findElement(By.xpath("//input[@name='phoneAreaCode']")).sendKeys("1");
+		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("1234567890");
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
-		Thread.sleep(2000);
-		String text = driver.findElement(By.xpath("//*[@id=\"ext-gen478\"]/div/table/thead/tr/td[1]/div")).getText();
-	    System.out.println(text);
-		driver.findElement(By.xpath("//div[contains(@class, 'x-grid3-col-partyId')]//a[1]")).click();
-		driver.findElement(By.xpath("//a[@class='subMenuButtonDangerous']")).click();
-		driver.findElement(By.linkText("Find Leads")).click();
-		driver.findElement(By.xpath("//input[@name='id']")).sendKeys("13529");
+		Thread.sleep(1000);
+		WebElement lead1 = driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId'])[1]/a"));
+		String lead1Id = lead1.getText();
+		System.out.println("lead: "+lead1Id);
+		lead1.click();
+		driver.findElement(By.className("subMenuButtonDangerous")).click();
+		Thread.sleep(100);
+		driver.findElement(By.partialLinkText("Find Leads")).click();//		15	Enter captured lead ID
+		driver.findElement(By.xpath("//label[text()='Lead ID:']/following::input")).sendKeys(lead1Id);//		16	Click find leads button
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
-		String text2 = driver.findElement(By.xpath("//div[text()='No records to display']")).getText();
-		System.out.println(text2);
-		if (text2.contains("No records to display")) {		
-		System.out.println("Lead Sucessfuly deleted");		
+		Thread.sleep(1000);
+		WebElement searchResult = driver.findElement(By.xpath("//div[text()='No records to display']"));
+		String expectedMsg = searchResult.getText();
+		String actualMsg = "No records to display";
+		if(expectedMsg.equalsIgnoreCase(actualMsg)) {
+			System.out.println("Lead Id: "+ lead1Id + " is deleted successfully" );				
+		}else {
+			System.out.println("Lead Id: "+ lead1Id + " is not deleted successfully");
 		}
-		else {
-		System.out.println("Lead is Not Deleted");
-		driver.quit();
+		driver.close();
 	    }
 
 }
